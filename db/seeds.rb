43c102ddd77a809b43b3edb557cc7ad6278e7cef
tommchenry <tommchenry@gmail.com>
2016-04-12 11:50:38 -0500

    Creates 10 item seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+10.times do
+  Comic.create!(title: Faker::Company.buzzword, img_url:Faker::Placeholdit.image , alt_text: Faker::Hipster.sentence, post_date:Faker::Date.backward(14))
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
