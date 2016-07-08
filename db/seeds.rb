bca915911e5768eb2a6037b8705773ce262c5824
tommchenry <tommchenry@gmail.com>
2016-07-08 12:13:21 -0500

    Bunch of style updates, seed file makes saner comic images.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 10.times do |num|
-  Comic.create!(title: Faker::Company.buzzword, img_url:Faker::Placeholdit.image , alt_text: Faker::Hipster.sentence, post_date:Faker::Time.between(num.days.ago, Date.today, :all))
+  title = Faker::Company.buzzword
+  Comic.create!(title: title, img_url:Faker::Placeholdit.image("600x906", "png", 'e9e9e9', '000', title), alt_text: Faker::Hipster.sentence, post_date:Faker::Time.between(num.days.ago, Date.today, :all))
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
