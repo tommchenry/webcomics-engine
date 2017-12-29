bec8a88a3da91a3fe4eb2b817899ca2f4fbf4ba3
Tom McHenry <tommchenry@gmail.com>
2017-12-29 15:47:56 -0600

    Update game links

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
-  get '/HorseMaster.html' => redirect('http://www.noncanon.online/games/HorseMaster.html')
+  get '/HorseMaster.html' => redirect('https://s3.amazonaws.com/noncanon-comics/games/HorseMaster.html')
 
   # this route is for the RSS feed
   get 'feed.rss', to: 'feeds#rss', :format => 'rss'
