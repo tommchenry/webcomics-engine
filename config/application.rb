5d75f56b6085d41786b299a392becb4e11b1cb25
Tom McHenry <tommchenry@gmail.com>
2020-02-24 15:17:42 -0600

    Update Ruby and Rails (#104)
    * Add .env to gitignore
    
    * Update all gems, get site to load
    
    * Get a bootable version of the site
    
    * Get versioning right please
    
    * Update travis script
    
    * Walk travis.yml back to basics
    
    * Add only postgresql
    
    * Add travis script
    
    * Trying another travis.yml
    
    * Add dist spec in travis
    
    * Add back update rubygems
    
    * Update ruby version in travis script

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module WebcomicEngine
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
+    config.public_file_server.enabled = true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,43 +1,42 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230220913) do
+ActiveRecord::Schema.define(version: 2017_12_30_220913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comics", force: :cascade do |t|
-    t.string   "title"
-    t.string   "alt_text"
+    t.string "title"
+    t.string "alt_text"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "url_slug"
+    t.string "url_slug"
     t.datetime "post_date"
-    t.boolean  "visible",            default: true
-    t.string   "keywords",           default: ""
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
+    t.boolean "visible", default: true
+    t.string "keywords", default: ""
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role",            default: "user"
+    t.string "role", default: "user"
   end
 
 end
